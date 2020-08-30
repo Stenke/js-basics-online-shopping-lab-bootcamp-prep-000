6336ab44a564952a4f41d17d8dd027e601268ebe
Stephen Enke <stephenenke@gmail.com>
2020-08-30 21:34:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,11 +53,10 @@ function removeFromCart(item) {
   }
 }
 function placeOrder(cardNumber) {
-  if (typeof(cardNumber) === "undefined") {
+   if (typeof(cardNumber) === "undefined"){
     return "Sorry, we don't have a credit card on file for you."
   } else {
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart = [];
-    return cart;
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
